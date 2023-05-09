b5fa205d3aa540f9fcc1cc2f27a4e936da79949c
Terrian Heck <terrianheck@Terrians-MacBook-Air.local>
2023-05-09 19:13:10 -0400

    adding new files

---
+++ practice/practiceScript.js
@@ -0,0 +1,36 @@
+// this is the first comment
+/* let person = {
+  name: "Mike",
+  age: 39,
+};
+
+person.age = 39;
+
+console.log(person.name);
+
+let firstName = "John";
+
+console.log(firstName);
+
+let selectedColors = ["red", "blue", "orange"];
+
+console.log(selectedColors[0]); 
+
+let selectMethod = ["red", "yellow"];
+selectMethod[3] = 4;
+console.log(selectMethod.length); */
+
+/*function greet(name, lastName) {
+  console.log("Hello " + name + " " + lastName);
+}
+
+greet("John", "smith");
+greet("Melony", "clark");
+*/
+
+function square(number) {
+  return number * number;
+}
+
+let number = square(2);
+console.log(number);
